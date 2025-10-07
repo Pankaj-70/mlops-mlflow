6a1c7686e2590d1dd44799a6b88a3406493bc205
Pankaj Sharma <grgnld3970@gmail.com>
2025-10-07 12:33:13 +0530

    dagshub, model registry

---
+++ src/wineOnDagsHub.py
@@ -0,0 +1,55 @@
+import mlflow
+from sklearn.datasets import load_wine
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix
+import matplotlib.pyplot as plt
+import seaborn as sns
+import dagshub
+
+
+
+dagshub.init(repo_owner='Pankaj-70', repo_name='mlops-mlflow', mlflow=True)
+mlflow.set_tracking_uri("https://dagshub.com/Pankaj-70/mlops-mlflow.mlflow")
+
+mlflow.set_experiment("Wine classification using Dagshub")
+# load dataset wine
+wine = load_wine()
+
+x = wine.data
+y = wine.target
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 42)
+
+
+max_depth = 10
+n_estimators = 12
+
+mlflow.autolog()
+
+with mlflow.start_run():
+  rf = RandomForestClassifier(max_depth=max_depth, n_estimators=n_estimators, random_state=42)
+  rf.fit(x_train, y_train)
+  
+  y_pred = rf.predict(x_test)
+
+  accuracy = accuracy_score(y_test, y_pred)
+
+  cm = confusion_matrix(y_test, y_pred)
+
+  plt.figure(figsize=(6,6))
+  sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=wine.target_names, yticklabels=wine.target_names) 
+  plt.ylabel('Actual')
+  plt.xlabel('Predicted')
+  plt.title('Confusion Matrix')
+
+
+  plt.savefig('Confusion-matrix.png')
+
+  mlflow.log_artifact(__file__)
+
+  mlflow.set_tags({'Author': 'Pankaj Sharma', 'Project': 'Wine Classification'})
+
+  example_input = x_test[:1]   
+  # mlflow.sklearn.log_model(rf, "Random Forest Model", input_example=example_input)
+
